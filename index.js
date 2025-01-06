0655e35d55db9fba9132b629c66b31a492bb3982
Sam Valentino <sammyvalentino7@gmail.com>
2025-01-06 17:08:08 -0500

    completed dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById("main");
+if (main) {
+    main.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Sam is the champion"; 
